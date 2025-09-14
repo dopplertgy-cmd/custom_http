2707774747c79aaa48f71bb1cb96bf420a963e9a
Dopplertgy <dopplertgy@gmail.com>
2025-09-13 21:35:02 -0700

    adding frontend

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require("express");
+import path from 'path';
+import express from 'express';
 const app = express();
 const PORT = 3000;
 
 // Respond to all GET requests
+const __dirname = path.resolve();
+
 app.get("/", (req, res) => {
-  res.send("Webpage content not available. Please visit tran-wong-llc.vercel.app for the correct page.");
+    res.sendFile(path.join(__dirname, '/frontend/index.html'));
+  //res.send("Webpage content not available. Please visit tran-wong-llc.vercel.app for the correct page.");
 });
 
 app.listen(PORT, () => {
